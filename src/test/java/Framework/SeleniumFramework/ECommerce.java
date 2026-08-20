526041838a742016a1e423e5d36b50c1834a28ad
vinobalan-kannan <vinobalan.kannan@gmail.com>
2026-08-20 20:41:09 +0530

    Update

--- src/test/java/Framework/SeleniumFramework/ECommerce.java
+++ src/test/java/Framework/SeleniumFramework/ECommerce.java
@@ -4,16 +4,20 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ECommerce {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		WebDriver driver = new ChromeDriver();
+
+		String items = "ZARA COAT 3";
 
 		driver.manage().window().maximize();
 
@@ -27,17 +31,68 @@ public class ECommerce {
 
 		driver.findElement(By.name("login")).click();
 
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 		List<WebElement> products = driver.findElements(By.cssSelector(".col-lg-4"));
 
-		WebElement prod = products.stream()
-				.filter(p -> p.findElement(By.cssSelector("b")).getText().equals("ADIDAS ORIGINAL")).findFirst()
-				.orElse(null);
+		WebElement prod = products.stream().filter(p -> p.findElement(By.cssSelector("b")).getText().equals(items))
+				.findFirst().orElse(null);
 
 		prod.findElement(By.cssSelector(".col-lg-4 button:last-of-type")).click();
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("ng-animating"))); //
 
-		// driver.quit();
+		driver.findElement(By.cssSelector("button[routerlink*=cart]")).click();
+
+		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cart h3"));
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		js.executeScript("window.scrollBy(0,500)");
+
+		cartProducts.stream().anyMatch(p -> p.getText().equalsIgnoreCase(items));
+
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Checkout']")));
+
+		driver.findElement(By.xpath("//button[text()='Checkout']")).click();
+
+		driver.findElement(By.xpath("//input[@placeholder='Select Country']")).sendKeys("India");
+
+		String country = "India";
+		List<WebElement> countryList = driver
+				.findElements(By.xpath("//input[@placeholder='Select Country'] / following-sibling::section/button"));
+
+		for (int i = 0; i < countryList.size(); i++) {
+
+			if (countryList.get(i).getText().equalsIgnoreCase(country)) {
+				countryList.get(i).click();
+			}
+		}
+
+		driver.findElement(By.cssSelector(".action__submit")).click();
+
+		driver.quit();
+
 	}
 
 }
+
+//Another ways 
+//for (int i = 0; i < products.size(); i++) {
+// 1. Store the web element once to avoid multiple DOM searches
+//WebElement currentProduct = products.get(i);
+//String productName = currentProduct.findElement(By.cssSelector("b")).getText().trim();
+//
+//// 2. Direct, clean boolean check
+//if (items.contains(productName)) {
+//	// 3. Click the add-to-cart button
+//	currentProduct.findElement(By.cssSelector(".col-lg-4 button:last-of-type")).click();
+//
+//	// 4. INDUSTRY STANDARD: Only wait AFTER an action causes the loading spinner to
+//	// appear
+//	wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".ng-animating")));
+//}
+//}
+
+//products.stream().filter(p -> items.contains(p.findElement(By.cssSelector("b")).getText()))
+//.forEach(prod -> prod.findElement(By.cssSelector(".col-lg-4 button:last-of-type")).click());
